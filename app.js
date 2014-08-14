c8c1d8142ca532d4516fd3a92799c58a7fe75d79
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2014-08-13 22:08:31 -0500

    app.js: Pass handler err status code in production mode

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
-        error: {},
+        error: { status: err.status }
     });
 });
 

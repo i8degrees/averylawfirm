5996ff5d0adce8a8c60f4a8680e63be8a7173a5b
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2014-08-18 13:43:37 -0500

    Introduce Markdown-based content loading for index && privacy pages

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,38 @@
 // Site (route) dependencies
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+// Initialize page content (pulled from Markdown files)
+var opts = { encoding: 'ascii' };
+
+var topics = {
+  about: {
+    title: 'About',
+    // id: 'about',
+    load: function load() { return fs.readFileSync('./data/pages/about.md', opts ); }
+  },
+  privacy: {
+    title: 'Privacy Policy',
+    // id: 'privacy',
+    load: function load() { return fs.readFileSync('./data/pages/privacy.md', opts ); }
+  }
+};
 
 router.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', { topic: topics['about'] } );
 });
 
 router.get('/index', function(req, res) {
-  res.render('index');
+  res.render('index', { topic: topics['about'] } );
 });
 
 router.get('/about', function(req, res) {
-  res.render('index');
+  res.render('index', { topic: topics['about'] } );
 });
 
 router.get('/privacy', function(req, res) {
-  res.render('privacy');
+  res.render('privacy', { topic: topics['privacy'] } );
 });
 
 router.get('/search_results', function(req, res) {
